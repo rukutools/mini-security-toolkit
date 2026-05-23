6c77436604e2a113461f2a880632194987f1cf22
rukutools <sruku721@gmail.com>
2026-05-23 16:22:51 +0530

    Create Ruku toolkit.py

---
+++ Ruku toolkit.py
@@ -0,0 +1,28 @@
+# MINI SECURITY TOOLKIT
+
+A beginner-friendly Python security and networking toolkit built using Termux + Kali Linux.
+
+## Features
+
+- Website Status Checker
+- Title Grabber
+- Header Checker
+- Website Report Saver
+- Multi Website Scanner
+- Response Time Checker
+- IP Info Lookup
+- Simple Port Checker
+
+## Built With
+
+- Python
+- Requests
+- BeautifulSoup
+- Colorama
+- Termux
+- Kali Linux
+
+## Installation
+
+```bash
+pip install requests beautifulsoup4 colorama --break-system-packages
